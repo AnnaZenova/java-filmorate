883704cc2b18a636783b8f1c5bcd170d5427935b
Anna <abzenova@yandex.ru>
2024-12-25 16:01:43 +0300

    Поправила CheckStyle

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate;
 
-import lombok.Builder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -29,7 +28,7 @@ class FilmorateApplicationTests {
 	void shouldCreateBeforeTest() {
 		film = new Film();
 		film.setName("Пупсик");
-		film.setReleaseDate(LocalDate.of(1700,11,12));
+		film.setReleaseDate(LocalDate.of(1700, 11, 12));
 		film.setDescription("Description");
 		film.setDuration(-100);
 
@@ -37,19 +36,19 @@ class FilmorateApplicationTests {
 
 		film2 = new Film();
 		film2.setName("Валидный");
-		film2.setReleaseDate(LocalDate.of(2025,11,12));
+		film2.setReleaseDate(LocalDate.of(2025, 11, 12));
 		film2.setDescription("Description val");
 		film2.setDuration(100);
 
 		user = new User();
-		user.setBirthday(LocalDate.of(2026,12,12));
+		user.setBirthday(LocalDate.of(2026, 12, 12));
 		user.setLogin("Pupsik");
 		user.setEmail("pupsik.ru");
 
 		user1 = new User();
 
 		user2 = new User();
-		user2.setBirthday(LocalDate.of(2024,12,12));
+		user2.setBirthday(LocalDate.of(2024, 12, 12));
 		user2.setLogin("Pupsikensky");
 		user2.setEmail("pupsik@.ru");
 
@@ -58,37 +57,37 @@ class FilmorateApplicationTests {
 	@Test
 	@DisplayName("Создаем фильм")
 	void shouldCreateMovie() {
-		assertEquals(filmController.create(film2),film2);
+		assertEquals(filmController.create(film2), film2);
 	}
 
 	@Test
 	@DisplayName("Создаем пустой фильм")
 	void shouldNotCreateEmptyMovie() {
-		assertThrows(NotFoundException.class,() -> filmController.create(film2),"Валидация не пройдена" );
+		assertThrows(NotFoundException.class, () -> filmController.create(film2), "Валидация не пройдена");
 	}
 
 	@Test
 	@DisplayName("Создаем юзера")
 	void shouldCreateUser() {
-		assertEquals(userController.create(user2),user2);
+		assertEquals(userController.create(user2), user2);
 	}
 
 	@Test
 	@DisplayName("Создаем пустого юзера")
 	void shouldNotCreateEmptyUser() {
-		assertThrows(NotFoundException.class,() -> userController.create(user1),"Валидация не пройдена" );
+		assertThrows(NotFoundException.class, () -> userController.create(user1), "Валидация не пройдена");
 	}
 
 	@Test
 	@DisplayName("Создаем юзера c некорректными данными")
 	void shouldNotCreateUserWithSuchData() {
-		assertThrows(NotFoundException.class,() -> userController.create(user),"Валидация не пройдена" );
+		assertThrows(NotFoundException.class, () -> userController.create(user), "Валидация не пройдена");
 	}
 
 	@Test
 	@DisplayName("Создаем фильм c некорректными данными")
 	void shouldNotCreatFilmWithSuchData() {
-		assertThrows(WrongDataException.class,() -> filmController.create(film),"Валидация не пройдена" );
+		assertThrows(WrongDataException.class, () -> filmController.create(film), "Валидация не пройдена");
 	}
 }
 
