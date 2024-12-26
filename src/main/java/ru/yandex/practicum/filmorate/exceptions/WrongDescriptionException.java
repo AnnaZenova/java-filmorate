d6e2a743215254da07bfeb4c3e3ca5d4461b81cd
Anna <abzenova@yandex.ru>
2024-12-26 14:58:54 +0300

    Внесла аннотации вместо if + CheckStyle

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/WrongDescriptionException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class WrongDescriptionException extends RuntimeException {
-    public WrongDescriptionException(String message) {
-        super(message);
-    }
-}
