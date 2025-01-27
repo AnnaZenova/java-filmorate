1d0466f0394405b4011d10fdf2dc53fdbd38cff6
Anna <abzenova@yandex.ru>
2025-01-27 15:50:35 +0300

    Внести изменения по REST и архитектуре

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -16,18 +16,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FilmController {
 
-    private final FilmService FilmService;
+    private final FilmService filmService;
 
     @GetMapping
     public Collection<Film> findAll() {
         log.info("Получен GET-запрос к эндпоинту: '/films' на получение фильмов");
-        return FilmService.findAll();
+        return filmService.findAll();
     }
 
     @GetMapping("/popular")
     public List<Film> showMostLikedFilms(@RequestParam(name = "count", defaultValue = "10") Integer count) {
         log.info("Получен GET-запрос к эндпоинту: '/films' на получение самого отлайканного фильма");
-        return FilmService.showMostLikedFilms(count);
+        return filmService.showMostLikedFilms(count);
     }
 
     @ResponseBody
@@ -35,7 +35,7 @@ public class FilmController {
     public Film create(@RequestBody @Valid Film film) {
         log.info("Получен POST-запрос к эндпоинту: '/films' на добавление фильма");
         // проверяем выполнение необходимых условий
-        film = FilmService.create(film);
+        film = filmService.create(film);
         return film;
     }
 
@@ -43,26 +43,26 @@ public class FilmController {
     public Film update(@RequestBody @Valid Film newFilm) {
         log.info("Получен PUT-запрос к эндпоинту: '/films' на обновление фильма");
         // проверяем необходимые условия
-        newFilm = FilmService.update(newFilm);
+        newFilm = filmService.update(newFilm);
         return newFilm;
     }
 
     @PutMapping("/{id}/like/{userId}")
     public void addLike(@PathVariable int id, @PathVariable int userId) {
         log.info("Получен PUT-запрос к эндпоинту: '/films' на добавление лайка");
-        FilmService.addLike(id, userId);
+        filmService.addLike(id, userId);
     }
 
     @DeleteMapping("/{id}/like/{userId}")
     public void deleteLike(@PathVariable int id, @PathVariable int userId) {
         log.info("Получен DELETE-запрос к эндпоинту: '/films' на удаление лайка фильма с ID={}", id);
-        FilmService.deleteLike(id, userId);
+        filmService.deleteLike(id, userId);
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable int id) {
         log.info("Получен DELETE-запрос к эндпоинту: '/films' на удаление фильма с ID={}", id);
-        FilmService.delete(id);
+        filmService.delete(id);
     }
 }
 
