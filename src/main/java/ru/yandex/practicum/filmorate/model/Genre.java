eb68b6441e6db6b16db254856e3962f73acbd1b2
Anna <abzenova@yandex.ru>
2025-05-11 13:24:17 +0300

    Добавить работу с БД+поправить поля+checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
