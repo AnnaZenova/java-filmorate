52effb057d7a921f67062d9841b72f93b7e3c83f
Vadim Pimenov <zigmundfreid89@yandex.ru>
2025-05-25 00:34:30 +0300

    Создать пакет recommendation:-> интерфейс RecommendationService и класс RecommendationServiceImp реализующий его.

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/recommendation/RecommendationService.java
@@ -0,0 +1,4 @@
+package ru.yandex.practicum.filmorate.service.recommendation;
+
+public interface RecommendationService {
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/recommendation/RecommendationServiceImp.java
@@ -0,0 +1,4 @@
+package ru.yandex.practicum.filmorate.service.recommendation;
+
+public class RecommendationServiceImp {
+}
